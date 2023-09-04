8e4f791155dca2f519f3b7d49c78d215a3d2d60a
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-04 21:15:36 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+let name = "Ravi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React From Scratch OH YEAHHH!
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+     <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsam, necessitatibus dignissimos eligendi quae iusto culpa quam enim veritatis accusamus odit labore ab praesentium rerum quibusdam illo illum ratione aut!
+      Ex laudantium praesentium totam quasi nostrum ab corporis quidem sunt necessitatibus, ullam numquam sint ad modi atque facilis reprehenderit doloremque quam earum quibusdam laboriosam saepe dignissimos. Voluptates nemo voluptatem cum!</p>
     </div>
+    </>
   );
 }
 
