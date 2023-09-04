98648597be7096c2f5c05c1fe704ddaa796683c0
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-04 20:02:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Scratch
+          Learn React From Scratch OH YEAHHH!
         </a>
       </header>
     </div>
