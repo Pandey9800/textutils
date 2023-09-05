ef1ccc3f10b5799ba6eb461946e937a8c4fe1ad0
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2023-09-05 22:44:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 let name = "Ravi";
+let hk = "Hare Krishna";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <li>Contact</li>
     </nav>
     <div className="container">
-     <h1>Hello {name}</h1>
+     <h1>{hk}</h1>
+     <h2>HELLO {name}</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias ipsam, necessitatibus dignissimos eligendi quae iusto culpa quam enim veritatis accusamus odit labore ab praesentium rerum quibusdam illo illum ratione aut!
       Ex laudantium praesentium totam quasi nostrum ab corporis quidem sunt necessitatibus, ullam numquam sint ad modi atque facilis reprehenderit doloremque quam earum quibusdam laboriosam saepe dignissimos. Voluptates nemo voluptatem cum!</p>
     </div>
